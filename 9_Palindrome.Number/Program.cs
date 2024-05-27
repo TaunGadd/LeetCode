ab4181449fe580670772678994a3c5a9f1092f58
Taun Gadd <taun.gadd@entelect.nl>
2024-05-27 11:22:44 +0200

    Add project files.

---
+++ 9_Palindrome.Number/Program.cs
@@ -0,0 +1,44 @@
+﻿var x = -121;
+var solution = new Solution();
+Console.WriteLine(solution.IsPalindrome(x));
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        var stringX = x.ToString();
+
+        for (int i = 0; i < stringX.Length; i++)
+        {
+            if (stringX[i] != stringX[^(i+1)])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsPalindromeNoString(int x)
+    {
+        if (x < 0) return false;
+
+        var list = new List<int>();
+
+        while (x > 0)
+        {
+            list.Add(x % 10);
+            x = x / 10;
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] != list[^(i + 1)])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
