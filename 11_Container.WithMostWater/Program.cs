23e287baf21e8fa2862468ed7a2d554673535de9
Taun Gadd <taun.gadd@entelect.nl>
2024-06-18 14:26:30 +0200

    Completed container problem

---
+++ 11_Container.WithMostWater/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] test1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+var solution = new Solution();
+
+Console.WriteLine(solution.MaxArea(test1));
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+
+        while (left < right)
+        {
+            var area = Math.Min(height[left], height[right]) * (right - left);
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
