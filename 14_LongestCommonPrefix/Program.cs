e8f54b2602446c3371d3703cf9143e5ceb43bb10
Taun Gadd <taun.gadd@entelect.nl>
2024-07-08 12:21:11 +0200

    Finished longest common prefix

---
+++ 14_LongestCommonPrefix/Program.cs
@@ -0,0 +1,34 @@
+﻿var solution = new Solution();
+var strings = new string[] {"ab", "a" };
+Console.WriteLine($"Answer: {solution.LongestCommonPrefix(strings)}");
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0) return string.Empty;
+        if (strs.Length == 1) return strs[0];
+
+        var longestPrefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++) {
+            var word = strs[i];
+
+            for (int j = 0; j < longestPrefix.Length; j++)
+            {
+                if (j >= word.Length)
+                {
+                    longestPrefix = word;
+                    break;
+                }
+
+                if (longestPrefix[j] != word[j])
+                {
+                    longestPrefix = longestPrefix.Substring(0,j);
+                }
+            }
+        }
+
+        return longestPrefix;
+    }
+}
---
+++ 4_MedianOfTwoArrays/Program.cs
@@ -0,0 +1,13 @@
+﻿var solution = new Solution();
+var arr1 = new[] { 1, 2, 3 };
+var arr2 = new[] { 4, 5, 6 };
+
+Console.WriteLine(solution.FindMedianSortedArrays(arr1, arr2));
+
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        nums1.
+    }
+}
